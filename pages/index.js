9f2b76051bca42108c0c2ec2f370e460cfe4acdf
Jayden Wu <996635308@qq.com>
2023-04-03 16:15:32 +0800

    feat(init)

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>我叫吴刚，作为一名前端开发工程师，对技术充满了热情。我的主要技术栈为Vue，React和Node。我会在这里分享我的生活点滴，不止于技术。</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
